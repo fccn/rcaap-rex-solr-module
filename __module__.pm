50c0b0393895a29bbd8af6d5edc86bef44665f68
Paulo Graça <paulo1978@gmail.com>
2022-05-25 12:06:20 +0100

    change setup process to make it dependent on package install_solr_service.sh file instead of the one in the module

--- __module__.pm
+++ __module__.pm
@@ -5,16 +5,16 @@ use Data::Dumper;
 use Rex::Commands::User;
 
 our %service_name = (
-   Debian => "solr",
-   Ubuntu => "solr",
-   CentOS => "solr",
-   Mageia => "solr",
+	Debian => "solr",
+	Ubuntu => "solr",
+	CentOS => "solr",
+	Mageia => "solr",
 );
 
 my %SOLR_CONF = ();
 Rex::Config->register_set_handler("solr" => sub {
-   my ($name, $value) = @_;
-   $SOLR_CONF{$name} = $value;
+	my ($name, $value) = @_;
+	$SOLR_CONF{$name} = $value;
 });
 
 
@@ -31,46 +31,48 @@ set solr => service => {
 
 
 task setup => sub {
-   my $source_gz = $SOLR_CONF{source_gz};
-   my $solr_port = $SOLR_CONF{service}->{port};
-   my $solr_dir = $SOLR_CONF{service}->{dir};
-   my $solr_log_dir = $SOLR_CONF{service}->{log_dir};
-   
-   my $tmp_dir = "/tmp";
-   $source_gz =~ m|(/[^/]+?)$|;
-   my $target_tgz = $tmp_dir.$1;
-   my $target_dir = '/opt/solr';
-  
-   # install required packages
-   update_package_db;
-   pkg "wget", ensure => "present";
-   pkg "lsof", ensure => "present";
+	my $source_gz = $SOLR_CONF{source_gz};
+	my $solr_port = $SOLR_CONF{service}->{port};
+	my $solr_dir = $SOLR_CONF{service}->{dir};
+	my $solr_log_dir = $SOLR_CONF{service}->{log_dir};
+
+	my $tmp_dir = "/tmp/solr_setup";
+	file $tmp_dir, ensure => "absent";	# delete content if any exists
+	file $tmp_dir, ensure => "directory";
+	$source_gz =~ m|(/[^/]+?)$|;
+	my $target_tgz = $tmp_dir.$1;
+	my $target_dir = '/opt/solr';
+
+	# install required packages
+	update_package_db;
+	pkg "wget", ensure => "present";
+	pkg "lsof", ensure => "present";
 
 	setup_user();
-	
-	#Build Properties
-	file $tmp_dir.'/install_solr_service.sh',
-		source => 'files/install_solr_service.sh',
-		mode  => 755;	
-		
+
 	file $solr_dir, ensure => "directory",
 		owner  => $SOLR_CONF{service}->{user},
 		group  => $SOLR_CONF{service}->{user};
 	file $solr_log_dir, ensure => "directory",
 		owner  => $SOLR_CONF{service}->{user},
 		group  => $SOLR_CONF{service}->{user};
-		
 
 	# download compressed source file
-	if (is_installed("wget")) {
-		Rex::Logger::info('Downloading '.$source_gz);
-		run 'wget '.$source_gz.' -O '.$target_tgz;
-		die('Error downloading: '. $source_gz) unless ($? == 0);
-	}   
-	
+	die ('wget required to install solr, please ensure it\'s present') unless (is_installed("wget"));
+
+	Rex::Logger::info('Downloading '.$source_gz);
+	run 'wget '.$source_gz.' -O '.$target_tgz;
+	die('Error downloading: '. $source_gz) unless ($? == 0);
+
+
+	#extract content to a tmp dir 
+	extract "$target_tgz", to => "$tmp_dir";
+
+	die ('unable to find install_solr_service.sh file') unless (is_file("$tmp_dir/solr*/bin/install_solr_service.sh"));
+
 	Rex::Logger::info("Installing solr...");
-	
-	run 'bash '.$tmp_dir.'/install_solr_service.sh '.$target_tgz.' -d '.$solr_dir.' -f -n -p '.$solr_port;
+
+	run 'bash '.$tmp_dir.'/solr*/bin/install_solr_service.sh '.$target_tgz.' -d '.$solr_dir.' -f -n -p '.$solr_port;
 	die('error installing solr service') unless ($? == 0);	
 	
 	file '/etc/default/solr.in.sh',
@@ -79,40 +81,34 @@ task setup => sub {
 		);
 };
 
-task start => sub {
-   my $service = $service_name{get_operating_system()};
-   service $service => "start";
-};
-
-task stop => sub {
-   my $service = $service_name{get_operating_system()};
-   service $service => "stop";
-};
-
-task restart => sub {
-   my $service = $service_name{get_operating_system()};
-   service $service => "restart";
-};
+my @service_actions = qw( start stop restart );
+foreach my $service_action (@service_actions) {
+	desc "$service_action action for Solr";
+	task "$service_action" => sub {
+		my $service = $service_name{get_operating_system()};
+		service $service => "$service_action";
+	};
+}
 
 sub is_running () {
-   my $solr_service = $SOLR_CONF{service}->{service};
-   run "ps -ef | grep -q $solr_service";   # only run if solr is running
-   die('Service '.$solr_service.' is not running.') unless ($? == 0);
+	my $solr_service = $SOLR_CONF{service}->{service};
+	run "ps -ef | grep -q $solr_service";   # only run if solr is running
+	die('Service '.$solr_service.' is not running.') unless ($? == 0);
 };
 
 #TODO: make it dynamic
 sub setup_user {
-  my $solr = $SOLR_CONF{service};
-  
-  group $solr->{user},
+	my $solr = $SOLR_CONF{service};
+
+	group $solr->{user},
 	ensure => "present";
-  
-  account $solr->{user},
-   ensure         => "present",  # default
-   home           => $solr->{dir},
-   comment        => 'Changed by solr rex module',
-   groups         => [ $solr->{user} ];   
-   #TODO: set /sbin/nologin as this user can't log
+
+	account $solr->{user},
+	ensure         => "present",  # default
+	home           => $solr->{dir},
+	comment        => 'Changed by solr rex module',
+	groups         => [ $solr->{user} ];   
+	#TODO: set /sbin/nologin as this user can't log
 };
 
 1;
